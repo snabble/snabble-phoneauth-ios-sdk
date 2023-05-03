b04a2786a74b42636e6c35d6188cb2cbf7bd81a0
Uwe Tilemann <uwe.tilemann@snabble.io>
2023-05-03 08:04:14 +0200

    added LoginService+Endpoint.swft

--- Sources/Core/Endpoint.swift
+++ Sources/Core/Endpoint.swift
@@ -91,31 +91,3 @@ public extension URLSession {
             .eraseToAnyPublisher()
     }
 }
-
-public extension Endpoint where Kind == EndpointKinds.Public, Response == PhoneResponse {
-     static func send(phoneNumber: String) -> Self {
-        let code = Int.random(in: 1000..<9999)
-        
-        return Endpoint(path: "delay/2", queryItems: [
-            URLQueryItem(name: "phoneNumber", value: phoneNumber.replacingOccurrences(of: "+", with: "00")),
-            URLQueryItem(name: "code", value: "\(code)")
-        ])
-    }
-}
-
-public extension Endpoint where Kind == EndpointKinds.Public, Response == Login {
-    static func loginWith(code: String) -> Self {
-        return Endpoint(path: "delay/2", queryItems: [
-            URLQueryItem(name: "code", value: code)
-        ])
-    }
-}
-
-public struct PhoneResponse: Decodable {
-    public let phoneNumber: String
-    public let code: String
-}
-
-public struct Login: Decodable {
-    public let code: String
-}
