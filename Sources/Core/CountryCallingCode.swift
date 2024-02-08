e41f8d399a44a804abe57c5dc88b87f838384ab7
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2024-02-08 09:53:42 +0100

    removes trunkprefix

--- Sources/Core/CountryCallingCode.swift
+++ Sources/Core/CountryCallingCode.swift
@@ -11,8 +11,7 @@ public struct CountryCallingCode {
     public static var `default`: [CountryCallingCode] = loadJSON("countries")
     
     public let countryCode: String // eg. DE, AT, CH
-    public let callingCode: UInt // eg. 49
-    public let trunkPrefix: UInt? // eg. 0 (see https://en.wikipedia.org/wiki/Trunk_prefix)
+    public let callingCode: UInt // eg. 49, 43
     
     public var flagSymbol: String? {
         countryCode.flagSymbol
@@ -20,39 +19,20 @@ public struct CountryCallingCode {
     
     public init(
         countryCode: String,
-        callingCode: UInt,
-        trunkPrefix: UInt? = nil
+        callingCode: UInt
     ) {
         self.countryCode = countryCode
         self.callingCode = callingCode
-        self.trunkPrefix = trunkPrefix
-    }
-    
-    private func numberRemovingTrunk(_ string: String) -> String {
-        guard let trunkPrefix, string.hasPrefix("\(trunkPrefix)") else {
-            return string
-        }
-        let start = string.index(string.startIndex, offsetBy: "\(trunkPrefix)".count)
-        return String(string[start...])
-    }
-    
-    public func internationalPhoneNumber(_ phoneNumber: String) -> String {
-        let phoneNumber = numberRemovingTrunk(phoneNumber)
-            .replacingOccurrences(of: " ", with: "")
-        return "+\(callingCode)\(phoneNumber)"
-    }
-    
-    public func prettyPrint(_ phoneNumber: String) -> String {
-        let phoneNumber = numberRemovingTrunk(phoneNumber)
-        return "+\(callingCode) \(phoneNumber)"
     }
 }
 
-extension CountryCallingCode: Identifiable, Hashable {
+extension CountryCallingCode: Identifiable {
     public var id: String {
         countryCode
     }
-    
+}
+
+extension CountryCallingCode: Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
@@ -62,14 +42,12 @@ extension CountryCallingCode: Decodable {
     private enum CodingKeys: String, CodingKey {
         case countryCode
         case callingCode
-        case trunkPrefix
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.countryCode = try container.decode(String.self, forKey: .countryCode)
         self.callingCode = try container.decode(UInt.self, forKey: .callingCode)
-        self.trunkPrefix = try container.decodeIfPresent(UInt.self, forKey: .trunkPrefix)
     }
 }
 
