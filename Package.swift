832ecf8596bb4e38c87c9c0b1e9733641871700b
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2024-02-14 11:36:13 +0100

    use snabble-network-ios-sdk

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,11 +16,9 @@ let package = Package(
             targets: ["SnabblePhoneAuth"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/lachlanbell/SwiftOTP", from: "3.0.2"),
+        .package(name: "SnabbleNetwork", url: "https://github.com/snabble/snabble-network-ios-sdk", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SnabblePhoneAuth",
             dependencies: [
@@ -33,23 +31,8 @@ let package = Package(
         ),
         .testTarget(
             name: "SnabblePhoneAuthTests",
-            dependencies: ["SnabbleNetwork", "SnabblePhoneAuth"],
+            dependencies: ["SnabblePhoneAuth"],
             path: "Tests/Core",
-            resources: [
-                .process("Resources")
-            ]
-        ),
-        .target(
-            name: "SnabbleNetwork",
-            dependencies: [
-                "SwiftOTP"
-            ],
-            path: "Sources/Network"
-        ),
-        .testTarget(
-            name: "SnabbleNetworkTests",
-            dependencies: ["SnabbleNetwork"],
-            path: "Tests/Network",
             resources: [
                 .process("Resources")
             ]
