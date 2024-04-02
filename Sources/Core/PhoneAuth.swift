86a286d67a69f461464101f00a47d175f995c4ef
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2024-04-02 14:36:34 +0200

    Add `changePhoneNumber` action (#16)

--- Sources/Core/PhoneAuth.swift
+++ Sources/Core/PhoneAuth.swift
@@ -11,7 +11,8 @@ import Combine
 
 public protocol PhoneAuthProviding {
     func startAuthorization(phoneNumber: String) async throws -> String
-    func login(phoneNumber: String, OTP: String) async throws -> AppUser?
+    func signIn(phoneNumber: String, OTP: String) async throws -> AppUser?
+    func changePhoneNumber(phoneNumber: String, OTP: String) async throws -> AppUser?
     func delete(phoneNumber: String) async throws
 }
 
@@ -44,78 +45,66 @@ public class PhoneAuth {
 }
 
 extension PhoneAuth: PhoneAuthProviding {
+    
+    private func useContinuation<Value, Response>(endpoint: Endpoint<Response>, receiveValue: @escaping (Response, CheckedContinuation<Value, any Error>) -> Void) async throws -> Value {
+        return try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
+            cancellable = networkManager.publisher(for: endpoint)
+                .mapHTTPErrorIfPossible()
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    switch completion {
+                    case .finished:
+                        break
+                    case let .failure(error):
+                        continuation.resume(throwing: error)
+                    }
+                    cancellable?.cancel()
+
+                } receiveValue: { response in
+                    receiveValue(response, continuation)
+                }
+        }
+    }
     public func startAuthorization(phoneNumber: String) async throws -> String {
         let endpoint = Endpoints.Phone.auth(
             phoneNumber: phoneNumber
         )
         
-        return try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = networkManager.publisher(for: endpoint)
-                .mapHTTPErrorIfPossible()
-                .receive(on: RunLoop.main)
-                .sink { completion in
-                    switch completion {
-                    case .finished:
-                        break
-                    case let .failure(error):
-                        continuation.resume(throwing: error)
-                    }
-                    cancellable?.cancel()
-
-                } receiveValue: { _ in
-                    continuation.resume(with: .success(phoneNumber))
-                }
+        return try await useContinuation(endpoint: endpoint) { _, continuation in
+            continuation.resume(with: .success(phoneNumber))
         }
     }
     
     @discardableResult
-    public func login(phoneNumber: String, OTP: String) async throws -> AppUser? {
-        let endpoint = Endpoints.Phone.login(
+    public func signIn(phoneNumber: String, OTP: String) async throws -> AppUser? {
+        let endpoint = Endpoints.Phone.signIn(
             phoneNumber: phoneNumber,
             OTP: OTP
         )
         
-        return try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = networkManager.publisher(for: endpoint)
-                .mapHTTPErrorIfPossible()
-                .receive(on: RunLoop.main)
-                .sink { completion in
-                    switch completion {
-                    case .finished:
-                        break
-                    case let .failure(error):
-                        continuation.resume(throwing: error)
-                    }
-                    cancellable?.cancel()
-
-                } receiveValue: { value in
-                    continuation.resume(with: .success(value?.fromDTO()))
-                }
+        return try await useContinuation(endpoint: endpoint) { response, continuation in
+            continuation.resume(with: .success(response?.fromDTO()))
+        }
+    }
+    
+    @discardableResult
+    public func changePhoneNumber(phoneNumber: String, OTP: String) async throws -> AppUser? {
+        let endpoint = Endpoints.Phone.changePhoneNumber(
+            phoneNumber: phoneNumber,
+            OTP: OTP
+        )
+        
+        return try await useContinuation(endpoint: endpoint) { response, continuation in
+            continuation.resume(with: .success(response?.fromDTO()))
         }
     }
     
     public func delete(phoneNumber: String) async throws {
         let endpoint = Endpoints.Phone.delete()
         
-        return try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = networkManager.publisher(for: endpoint)
-                .mapHTTPErrorIfPossible()
-                .receive(on: RunLoop.main)
-                .sink { completion in
-                    switch completion {
-                    case .finished:
-                        break
-                    case let .failure(error):
-                        continuation.resume(throwing: error)
-                    }
-                    cancellable?.cancel()
-
-                } receiveValue: { value in
-                    continuation.resume(with: .success(value))
-                }
+        return try await useContinuation(endpoint: endpoint) { response, continuation in
+            continuation.resume(with: .success(response))
         }
     }
 }
